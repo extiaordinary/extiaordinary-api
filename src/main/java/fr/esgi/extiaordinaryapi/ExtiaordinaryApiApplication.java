0dd84157e3448a88b565f3530393dd5b94562916
saziz <shan.azizh@gmail.com>
2023-03-01 20:14:28 +0100

    initial commit

---
+++ src/main/java/fr/esgi/extiaordinaryapi/ExtiaordinaryApiApplication.java
@@ -0,0 +1,13 @@
+package fr.esgi.extiaordinaryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtiaordinaryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExtiaordinaryApiApplication.class, args);
+    }
+
+}
