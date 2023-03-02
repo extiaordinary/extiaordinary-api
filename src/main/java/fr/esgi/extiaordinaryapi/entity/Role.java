0b1f0f9de97b31cad6db78b54c8b94a4a9282392
saziz <shan.azizh@gmail.com>
2023-03-02 11:04:18 +0100

    fix: unique email

--- src/main/java/fr/esgi/extiaordinaryapi/entity/Role.java
+++ src/main/java/fr/esgi/extiaordinaryapi/entity/Role.java
@@ -1,12 +1,6 @@
 package fr.esgi.extiaordinaryapi.entity;
 
 public enum Role {
-    USER("user"),
-    COACH("coach");
-
-    private String label;
-
-    Role(String label) {
-        this.label = label;
-    }
+    USER,
+    COACH
 }
--- src/main/java/fr/esgi/extiaordinaryapi/entity/User.java
+++ src/main/java/fr/esgi/extiaordinaryapi/entity/User.java
@@ -26,6 +26,7 @@ public class User implements UserDetails {
     private UUID id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
     private String password;
     private LocalDate dateOfBirth;
